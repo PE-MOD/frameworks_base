6518041ff9032099bb199feed9175d900d448e3c
ezio84 <brabus84@gmail.com>
2019-11-29 23:46:55 +0100

    Screenrecord: remember last recording options
    Change-Id: Ib54106bfa0f2c2debba16c6fbb15b4d3d1c36683
    Signed-off-by: kunal <kunalshah912@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4604,6 +4604,30 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
         /**
+         * Screenrecord: enable mic
+         * @hide
+         */
+        public static final String SCREENRECORD_ENABLE_MIC = "screenrecord_enable_mic";
+
+        /**
+         * Screenrecord: show taps
+         * @hide
+         */
+        public static final String SCREENRECORD_SHOW_TAPS = "screenrecord_show_taps";
+
+        /**
+         * Screenrecord: show stop dot
+         * @hide
+         */
+        public static final String SCREENRECORD_STOP_DOT = "screenrecord_stop_dot";
+
+        /**
+         * Screenrecord: low quality
+         * @hide
+         */
+        public static final String SCREENRECORD_LOW_QUALITY = "screenrecord_low_quality";
+
+        /**
          * Whether to disable showing arrows in statusbar network traffic indicators
          * @hide
          */
