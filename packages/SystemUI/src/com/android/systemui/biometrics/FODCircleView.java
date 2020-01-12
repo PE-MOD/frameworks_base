1dfb3ff000c27cb11eeb565128155a20e8b23bc2
kunal <kunalshah912@gmail.com>
2020-01-12 18:35:03 +0530

    FOD: patch wakelock issues
    Change-Id: I50b90e274ea7ee0bea6b45f2a3dc78205ba63ed4
    Signed-off-by: kunal <kunalshah912@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -240,6 +240,17 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     }
 
     @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+        if (mIsCircleShowing) {
+            dispatchPress();
+        } else {
+            dispatchRelease();
+        }
+    }
+
+    @Override
     public boolean onTouchEvent(MotionEvent event) {
         float x = event.getAxisValue(MotionEvent.AXIS_X);
         float y = event.getAxisValue(MotionEvent.AXIS_Y);
@@ -284,7 +295,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     public void dispatchPress() {
         IFingerprintInscreen daemon = getFingerprintInScreenDaemon();
         try {
-            daemon.onPress();
+            daemon.onPress(mIsDreaming ? 1 :0);
         } catch (RemoteException e) {
             // do nothing
         }
